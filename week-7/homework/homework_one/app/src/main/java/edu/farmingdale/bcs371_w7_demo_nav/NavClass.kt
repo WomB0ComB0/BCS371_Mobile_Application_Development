84cfe14e842f90c43e6f1c104eca0f0c6f69b415
Mike Odnis <airwolf635@gmail.com>
2024-10-12 10:50:55 -0400

    feat: done with week-7 homework 1

--- week-7/homework/homework_one/app/src/main/java/edu/farmingdale/bcs371_w7_demo_nav/NavClass.kt
+++ week-7/homework/homework_one/app/src/main/java/edu/farmingdale/bcs371_w7_demo_nav/NavClass.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Slider
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
